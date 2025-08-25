bcf19e4f81b3037368d9e7da575888e0199db5a1
piratchai <pong_bb1@hotmail.com>
2025-08-25 13:58:50 +0700

    Add Windows runner files for Flutter application
    - Created runner executable manifest to support DPI awareness and compatibility with Windows 10 and 11.
    - Implemented utility functions for console creation, command line argument retrieval, and UTF-16 to UTF-8 string conversion in utils.cpp and utils.h.
    - Developed Win32Window class for managing a high DPI-aware window, including message handling, theme updates, and child content management in win32_window.cpp and win32_window.h.

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_empty_application_1
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: CounterPage());
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Counter: $_counter')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
